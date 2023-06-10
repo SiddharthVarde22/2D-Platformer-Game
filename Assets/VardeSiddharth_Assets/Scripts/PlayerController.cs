7a2e5f942b8ffda356098cabb1c14de9392b1c0d
SiddharthVarde22 <21stargames@gmail.com>
2023-06-11 00:50:21 +0530

    Added platform in scene, setting the animation according to the user input and fliping the player with user input

--- Assets/VardeSiddharth_Assets/Scripts/PlayerController.cs
+++ Assets/VardeSiddharth_Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Animator playerAnimator;
+
+    float speed;
+    Vector3 playerLocalScale;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        speed = Input.GetAxisRaw("Horizontal");
+
+        playerAnimator.SetFloat("Speed", Mathf.Abs(speed));
+
+        playerLocalScale = transform.localScale;
+
+        if(speed < 0)
+        {
+            playerLocalScale.x = -1 * Mathf.Abs(playerLocalScale.x);
+        }
+        else if(speed > 0)
+        {
+            playerLocalScale.x = Mathf.Abs(playerLocalScale.x);
+        }
+
+        transform.localScale = playerLocalScale;
     }
 }
